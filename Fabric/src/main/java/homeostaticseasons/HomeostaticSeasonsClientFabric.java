31fa6a4fe244c581c840abd2d5389db092f95f88
Mitch M <8550058+mitchjm@users.noreply.github.com>
2026-01-18 22:19:07 -0500

    Added call for SyncBiomeColormap.handle() to the Fabric Client Fixes #2

--- Fabric/src/main/java/homeostaticseasons/HomeostaticSeasonsClientFabric.java
+++ Fabric/src/main/java/homeostaticseasons/HomeostaticSeasonsClientFabric.java
@@ -1,8 +1,10 @@
 package homeostaticseasons;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 
 import homeostaticseasons.event.ClientEventListener;
+import homeostaticseasons.network.SyncBiomeColormap;
 
 public class HomeostaticSeasonsClientFabric implements ClientModInitializer {
 
@@ -10,6 +12,20 @@ public class HomeostaticSeasonsClientFabric implements ClientModInitializer {
     public void onInitializeClient() {
         HomeostaticSeasonsClient.init();
         ClientEventListener.init();
+
+        ClientPlayNetworking.registerGlobalReceiver(SyncBiomeColormap.TYPE, (payload, ctx) -> {
+            ctx.client().execute(() -> {
+                payload.handle(ctx.player());
+
+                var mc = ctx.client();
+                if (mc.level != null) {
+                    mc.level.clearTintCaches();
+                }
+                if (mc.levelRenderer != null) {
+                    mc.levelRenderer.allChanged();
+                }
+            });
+        });
     }
 
 }
