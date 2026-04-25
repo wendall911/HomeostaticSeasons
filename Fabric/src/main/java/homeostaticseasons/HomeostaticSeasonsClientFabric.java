4d985aaaddf3f3f01206d70856fa6a90318b3e81
Wendall Cada <wendallc@83864.com>
2026-04-25 10:53:26 -0700

    Update to refresh renders for seasonal effects at Midnight. Changes season to update at midnight. If using mod like BetterDays with the sleep feature enabled, will refresh while player is sleeping. Partially addresses #3

--- Fabric/src/main/java/homeostaticseasons/HomeostaticSeasonsClientFabric.java
+++ Fabric/src/main/java/homeostaticseasons/HomeostaticSeasonsClientFabric.java
@@ -3,7 +3,6 @@ package homeostaticseasons;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 
-import homeostaticseasons.event.ClientEventListener;
 import homeostaticseasons.network.SyncBiomeColormap;
 
 public class HomeostaticSeasonsClientFabric implements ClientModInitializer {
@@ -11,7 +10,6 @@ public class HomeostaticSeasonsClientFabric implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         HomeostaticSeasonsClient.init();
-        ClientEventListener.init();
 
         ClientPlayNetworking.registerGlobalReceiver(SyncBiomeColormap.TYPE, (payload, ctx) -> {
             ctx.client().execute(() -> {
